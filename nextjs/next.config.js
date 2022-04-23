eaa4ea48ec952232be8b399c2595039303793ba9
Dandrum <daniel.schmidt89@freenet.de>
2022-04-23 03:42:16 +0200

    Update Security Headers

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -7,10 +7,11 @@ const {i18n} = require("./next-i18next.config");
 
 const cspConfig = {
   defaultDomain: process.env.CSP_DEFAULT_DOMAIN,
-  apiDomains: process.env.CSP_API_DOMAINS,
+  connectDomains: process.env.CSP_API_DOMAINS,
   scriptDomains: process.env.CSP_SCRIPT_DOMAINS,
   imageDomains: process.env.CSP_IMAGE_DOMAINS,
-  styleDomains: process.env.CSP_STYLE_DOMAINS
+  styleDomains: process.env.CSP_STYLE_DOMAINS,
+  frameDomains: process.env.CSP_FRAME_DOMAINS,
 };
 
 let prod = process.env.APP_ENV === "production";
@@ -19,7 +20,8 @@ function getCsp() {
   let csp = `default-src 'self' ${cspConfig.defaultDomain};`;
   csp += `base-uri 'self';`;
   csp += `form-action 'self';`;
-  csp += `connect-src 'self' ${cspConfig.apiDomains};`;
+  csp += `connect-src 'self' ${cspConfig.connectDomains};`;
+  csp += `frame-src 'self' ${cspConfig.frameDomains};`;
   csp += `script-src 'self' ${cspConfig.scriptDomains} ${prod ? "" : "'unsafe-eval'"};`; // NextJS requires 'unsafe-eval' in dev (faster source maps)
   csp += `style-src 'self' 'unsafe-inline' data: ${cspConfig.styleDomains};`; // NextJS requires 'unsafe-inline'
   csp += `img-src 'self' data: blob: ${cspConfig.imageDomains} ;`;
@@ -32,12 +34,36 @@ function getCsp() {
 
 const securityHeaders = [
   {
+    key: 'Cache-Control',
+    value: 'public, max-age=31536000, must-revalidate',
+  },
+  {
     key: 'X-DNS-Prefetch-Control',
     value: 'on'
   },
   {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
     key: 'Permissions-Policy',
-    value: 'geolocation=()'
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
   },
   {
     key: 'Content-Security-Policy',
